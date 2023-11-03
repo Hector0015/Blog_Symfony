b656d618baf45d0d84c4a3a5a66e75c12ccc7c7d
Hector0015 <alufog5923@ieselcaminas.org>
2023-11-03 10:40:59 +0100

    Empezar formulario de contacto

--- Blog-symfony/src/Controller/PageController.php
+++ Blog-symfony/src/Controller/PageController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
-    #[Route('/', name: 'app_page')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('page/index.html.twig', [
@@ -19,6 +19,27 @@ class PageController extends AbstractController
     public function about(): Response
     {
         return $this->render('page/about.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+    #[Route('/service', name: 'service')]
+    public function service(): Response
+    {
+        return $this->render('page/service.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+    #[Route('/team', name: 'team')]
+    public function team(): Response
+    {
+        return $this->render('page/team.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+    #[Route('/why', name: 'why')]
+    public function why(): Response
+    {
+        return $this->render('page/why.html.twig', [
             'controller_name' => 'PageController',
         ]);
     }
